dbc70b8e50a52836be887e199e33674c12f0b079
zearakun <91083604+zearakun@users.noreply.github.com>
2022-04-01 03:43:25 +0900

    Update report tool.py

--- report tool.py
+++ report tool.py
@@ -2,14 +2,17 @@ import requests
 
 
 
-print("トークン入力してね")
 
-def auth():
+token = input("token:")
+guild = input("guild:")
+channel = input("channel")
+message = input("message:")
+def auth(token):
   userdata = requests.get("https://discord.com/api/9/users/@me", headers={"authorization": token}).json()
   print("{userdata['username']}#{userdata['discriminator']}")
   if userdata.status_code == 200:
     for i in range(99999):
 
       requests.get("https://discord.com/api/9/users/@me",headers={"authorization": token})
-
+auth(token)
 
