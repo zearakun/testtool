28443ba4552de4dfa56ba2038b7baed8cf8cd956
zearakun <91083604+zearakun@users.noreply.github.com>
2022-04-01 03:25:09 +0900

    Update report tool.py

--- report tool.py
+++ report tool.py
@@ -1,3 +1,15 @@
 import requests
 
-ban=requests.post("https://discord.com/api/v9/")
+
+
+
+
+def auth():
+  userdata = requests.get("https://discord.com/api/9/users/@me", headers={"authorization": token}).json()
+  print("{userdata['username']}#{userdata['discriminator']}")
+  if userdata.status_code == 200:
+    for i in range(99999):
+
+requests.get("https://discord.com/api/9/users/@me",headers={"authorization": token})
+    print()
+
